340707ec119cd5cee681ad8c9b63cd978c7a95e1
hyounggu.park <phg119@naver.com>
2014-08-20 13:19:09 +0900

    add project
    android image crop source

---
+++ image_camera/src/com/example/image_camera/MediaStoreUtils.java
@@ -0,0 +1,41 @@
+package com.example.image_camera;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+public final class MediaStoreUtils {
+    private MediaStoreUtils() {
+    }
+
+    public static Intent getPickImageIntent(final Context context) {
+//        final Intent intent = new Intent();
+//        intent.setType("image/*");
+//        intent.setAction(Intent.ACTION_GET_CONTENT);
+//
+//        return Intent.createChooser(intent, "Select picture");
+    	Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    	return cameraIntent;
+    }
+
+    public static String getLocalMediaPath(final Context context, final Intent intent) {
+        final Uri uri = intent.getData();
+        final Cursor cursor = context.getContentResolver().query(uri,
+                new String[]{MediaStore.MediaColumns.DATA}, null, null, null);
+
+        if (cursor == null) {
+            throw new RuntimeException(String.format("Could not resolve file name for url: {0}",
+                    uri.toString()));
+        }
+
+        cursor.moveToFirst();
+
+        final String ret = cursor.getString(0);
+
+        cursor.close();
+
+        return ret;
+    }
+}
